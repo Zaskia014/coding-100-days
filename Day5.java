c3ffd45c0f1e6235e10f7d8709f889d6965a3715
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-12 20:30:26 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,20 @@
+package coding100days;
+
+public class Day5 {
+
+    public static void main(String[] args) {
+
+        byte umur = 20;
+        System.out.println("umur byte) : " + umur);
+
+        short jumlahmahasiswa = 100;
+        System.out.println("jumlah (short) : " + jumlahmahasiswa);
+
+        int gaji = 100000;
+        System.out.println("gaji (int) : " + gaji);
+
+        long jarakrumah = 384400000L;
+        System.out.println("jarak (long) :  " + jarakrumah);
+    }
+
+}
