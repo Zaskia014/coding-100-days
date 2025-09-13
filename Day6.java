d94b687d0fe4c4767eb9befbcc991e257802f8cd
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-13 19:30:35 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,17 @@
+package coding100days;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+    
+        float angkaFloat = 3.14f;   // wajib pakai 'f'
+        
+        // Tipe data double
+        double angkaDouble = 3.14159265358979; // default pecahan
+        
+        // Menampilkan hasil
+        System.out.println("Nilai float: " + angkaFloat);
+        System.out.println("Nilai double: " + angkaDouble);
+    }
+}
+
