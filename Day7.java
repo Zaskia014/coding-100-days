ad91a791e770c3e932cc466c7c05d3e0ce4f95fa
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-14 19:43:19 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,20 @@
+package coding100days;
+
+public class Day7 {
+
+    public static void main(String[] args) {
+        
+        char Jenis_kelamin = 'P';
+        
+        boolean laki_laki = false;
+        boolean Perempuan = true;
+        
+        System.out.println("jenis kelamin : " + Jenis_kelamin);
+        System.out.println("laki laki : " + laki_laki);
+        System.out.println("perempuan : " + Perempuan);
+        
+        
+
+    }
+    
+}
