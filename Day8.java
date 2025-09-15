99587db7235f75dc488da27a19111a56a244eca1
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-15 21:22:34 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,23 @@
+
+package coding100days;
+
+
+public class Day8 {
+    public static void main(String[] args) {
+        
+        String nama = "Zaskia";
+        String kampus = "universitas sulawesi barat";
+        String fakultas = "Teknik";
+        String prodi = "informatika";
+        
+        System.out.println("Nama : " + nama);
+        System.out.println("Kampus : " + kampus);
+        System.out.println("Fakultas : " + fakultas);
+        System.out.println("Prodi : " + prodi);
+                
+                
+           
+  
+    }
+}
+   
