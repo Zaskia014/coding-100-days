cc3f9b7515505364630a6bfda032124e675b5ce2
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-16 20:13:02 +0800

    Update Day9.java

--- Day9.java
+++ Day9.java
@@ -6,15 +6,20 @@ public class Day9 {
 
         // deklarasi saja
         String nama;
+        int nilai;
+        
 
         // inisialisasi (beri nilai pertama kali)
         nama = "Zaskia";
-
+        nilai = 4;
         System.out.println("Nama awal: " + nama);
+        System.out.println("Nilai awal: " + nilai);
 
         // update (ubah nilainya)
         nama = "Kia";
+        nilai = 2;
         System.out.println("Nama setelah update: " + nama);
+        System.out.println("Nilai setelah update: " + nilai);
     }
 }
 
