e9752462d0be34dc7215c32ba8dab091b3d8e148
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-16 20:08:11 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,20 @@
+package coding100days;
+
+public class Day9 {
+
+    public static void main(String[] args) {
+
+        // deklarasi saja
+        String nama;
+
+        // inisialisasi (beri nilai pertama kali)
+        nama = "Zaskia";
+
+        System.out.println("Nama awal: " + nama);
+
+        // update (ubah nilainya)
+        nama = "Kia";
+        System.out.println("Nama setelah update: " + nama);
+    }
+}
+
