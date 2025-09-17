4940901bc591cc0c3509505af1f30a1571a9e793
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-17 20:48:17 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,10 @@
+package coding100days;
+
+public class Day10 {
+
+    public static void main(String[] args) {
+        final double tb = 154.2;
+        System.out.println("tinggi badan saya  : " + tb);
+        // tb = 154.2; Ketika dia constanta maka tidak bisa di update
+    }
+} 
