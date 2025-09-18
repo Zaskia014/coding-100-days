a5a49e3eb2515e504051371ae2388554d6f49ed0
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-18 20:53:53 +0800

    Day11.java

---
+++ Day11/Day11.java
@@ -0,0 +1,19 @@
+package coding100days;
+
+import java.util.Scanner;
+
+public class Day11 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nama : ");
+        String nama = input.nextLine();
+
+        System.out.print("Umur : ");
+        short umur = input.nextShort();
+        System.out.println("nama saya : " + nama + " dan umur saya " + umur);
+
+    }
+}
+    
+
