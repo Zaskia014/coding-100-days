7272ff0464e65e35c1d271bfa863ba6eee4d77ae
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-21 20:06:05 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,18 @@
+package coding100days;
+
+public class Day14 {
+
+    public static void main(String[] args) {
+
+        String nilai = "1410";
+        String decimal = "20.15";
+
+        int angka = Integer.parseInt(nilai);
+        double Decimal = Double.parseDouble(decimal);
+
+        System.out.println("hasil : " + nilai);
+        System.out.println("hasil : " + decimal);
+
+    }
+
+}
