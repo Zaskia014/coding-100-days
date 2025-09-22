a2fcad5375ca0f3a6ccef0f8bb19a3c62a40ca26
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-22 20:37:58 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,15 @@
+package coding100days;
+
+public class Day15 {
+
+    public static void main(String[] args) {
+
+        //operator arikmatika
+        int angka1 = 40;
+        int angka2 = 20;
+        System.out.println("hasil penjumlahan : " + (angka1 + angka2));
+        System.out.println("hasil pengurangan : " + (angka1 - angka2));
+
+    }
+
+}
