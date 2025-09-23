bec9ee6e3ecc6306d3395aa00f594738a0403866
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-23 22:03:54 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,24 @@
+
+package coding100days;
+
+public class Day16 {
+    public static void main(String[] args) {
+        
+        //operator arikmatika
+        
+        int angka1 = 10;
+        int angka2 =  2;
+        
+        int perkalian = angka1 * angka2;
+        int pembagian = angka1 / angka2;
+        
+        System.out.println("hasil : " + perkalian);
+        System.out.println("hasil : " + pembagian);
+                
+        
+       
+        
+
+    }
+    
+}
