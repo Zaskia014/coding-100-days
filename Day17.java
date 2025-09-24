3b1a31619731e1985ac419b95e7602e22008f6ea
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-24 13:46:22 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,14 @@
+package coding100days;
+
+
+public class Day17 {
+    public static void main(String[] args) {
+        
+        int a = 27;
+        int b = 4;
+        
+        int hasil = a % b;
+        System.out.println("hasil sisa bagi : " + hasil);
+    }
+    
+}
