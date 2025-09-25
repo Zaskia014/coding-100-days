41f4e6819f060d15d2fdb3770b5245448c84abf7
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-25 21:46:56 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,15 @@
+package coding100days;
+
+public class Day18 {
+
+    public static void main(String[] args) {
+        
+        //Konversi Otomatis (Dari Tipe Data Kecil ke Besar).
+
+        int nilai = 100;       
+        double angkadouble = nilai;  
+
+        System.out.println("Nilai int: " + nilai);
+        System.out.println("Nilai double (otomatis): " + angkadouble);
+    }
+}
