3a535455717901a2451f0cd3fa5401e0475de890
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-26 21:39:46 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,16 @@
+package coding100days;
+
+public class Day19 {
+    public static void main(String[] args) {
+        
+        //Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil).
+        
+        double angka = 9.34;
+        int angkaint = (int) angka;
+        
+        System.out.println("angka asli :" + angka);
+        System.out.println("angka konversi :" + angkaint);
+        
+    }
+    
+}
