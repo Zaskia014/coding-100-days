98e253248debfb9c7a3e9d60197c3b14eaf17991
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-09-27 20:51:09 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,18 @@
+package coding100days;
+
+public class Day20 {
+
+    public static void main(String[] args) {
+
+        int angka = 123;
+        String strangka = String.valueOf(angka);
+
+        double decimal = 43.54;
+        String strdecimal = String.valueOf(decimal);
+
+        System.out.println("hasil : " + strangka);
+        System.out.println("hasil : " + strdecimal);
+
+    }
+
+}
