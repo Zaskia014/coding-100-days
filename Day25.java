230aa80b6a7dec5d725d5588992d48e2e959905b
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-10-02 23:11:23 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,18 @@
+package coding100days;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+
+        int a = 7;
+        int b = 12;
+
+        System.out.println("Nilai awal: a = " + a + ", b = " + b);
+
+        a++;
+        b--;
+
+        System.out.println("Setelah a++ : a = " + a);
+        System.out.println("Setelah b-- : b = " + b);
+    }
+}
