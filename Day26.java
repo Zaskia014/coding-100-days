c9260b6c503db5036cc73aeaa12f909b84daf59e
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-10-03 20:45:24 +0800

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,17 @@
+package coding100days;
+
+public class Day26 {
+
+    public static void main(String[] args) {
+
+        int x = 10; 
+        System.out.println("Nilai awal x = " + x);
+
+        x += 5;  
+        System.out.println( x);
+
+        x -= 3;  
+        System.out.println( x);
+    }
+
+}
