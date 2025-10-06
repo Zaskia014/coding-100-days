fb85987ffa55073bab93672759089a71788d2c63
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-10-06 22:22:40 +0800

    Day29.java

---
+++ Day29.java
@@ -0,0 +1,12 @@
+package coding100days;
+
+public class Day29 {
+    public static void main(String[] args) {
+        
+        int a = 20;
+        int b = 30;
+        
+        System.out.println("nilai lebih besar :" + (a < b));
+        System.out.println("nilai lebih kecil :" + (a > b));
+    }
+}
