19808ea712b632cff061c19596dc2463dcebe47d
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-10-07 23:06:27 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,14 @@
+package coding100days;
+
+public class Day30 {
+
+    public static void main(String[] args) {
+
+        int nilai1 = 10;
+        int nilai2 = 8;
+
+        System.out.println("hasil : " + (nilai1 <= nilai2));
+        System.out.println("hasil : " + (nilai1 >= nilai2));
+
+    }
+}
