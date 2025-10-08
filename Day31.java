9957fe60787274add2f49fcc6ccb8b150a8dc59c
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-10-08 22:14:10 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,18 @@
+package coding100days;
+
+public class Day31 {
+
+    public static void main(String[] args) {
+
+        int nilai_uts = 90;
+        int absensi = 75;
+
+        if (nilai_uts > 85 && absensi > 70) {
+            System.out.println("lulus");
+        } else {
+            System.out.println("tidak lulus");
+        }
+
+    }
+
+}
