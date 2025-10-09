63b5df4c71c88f8cd4ee07fe1f1826b27ef8bd36
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-10-09 21:51:17 +0800

    Day32.java

---
+++ Day32.java
@@ -0,0 +1,17 @@
+package coding100days;
+
+public class Day32 {
+
+    public static void main(String[] args) {
+
+        int nilai_uts = 60;
+        int absensi = 80;
+
+        if (nilai_uts > 70 || absensi > 75) {
+            System.out.println("Lulus");
+        } else {
+            System.out.println("Tidak Lulus");
+        }
+
+    }
+}
