f8a97eddcf13eb759eb888b4554485d6ff6807fa
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-10-10 23:08:44 +0800

    Day33.java

---
+++ Day33.java
@@ -0,0 +1,15 @@
+package coding100days;
+
+public class Day33 {
+
+    public static void main(String[] args) {
+        
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("nilai kebalikan  : " + !a);        
+        System.out.println("nilai kebalikan  : " + !b);        
+
+    }
+
+}
