ac25175d05ff6217e6f0d52710991c01f9c72374
Zaskia <147218049+Zaskia014@users.noreply.github.com>
2025-10-12 21:53:31 +0800

    Day35.java

---
+++ Day35.java
@@ -0,0 +1,16 @@
+package coding100days;
+
+public class Day35 {
+
+    public static void main(String[] args) {
+
+        int nilai = 60;
+
+        if (nilai >= 75) {
+            System.out.println("Lulus");
+        } else {
+            System.out.println("Tidak Lulus");
+        }
+    }
+
+}
